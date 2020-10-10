f8ff6c74ef4b50993b946d31d11203e804d710dd
nauman007 <iemnauman.akram@gmail.com>
2020-10-10 15:30:10 +0500

    used mobilenet inference

--- ops/models.py
+++ ops/models.py
@@ -141,7 +141,7 @@ class TSN(nn.Module):
                 from ops.temporal_shift import TemporalShift
                 for m in self.base_model.modules():
                     if isinstance(m, InvertedResidual) and len(m.conv) == 8 and m.use_res_connect:
-                        if self.print_spec:
+                        if self.is_shift:
                             print('Adding temporal shift... {}'.format(m.use_res_connect))
                         m.conv[0] = TemporalShift(m.conv[0], n_segment=self.num_segments, n_div=self.shift_div)
             if self.modality == 'Flow':
