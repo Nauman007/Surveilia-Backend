3897eab0d8d178146bb88809d2e8f7356f90da9f
nauman007 <iemnauman.akram@gmail.com>
2020-10-23 22:56:40 +0500

    cleaned up code

--- realTimeInference.py
+++ realTimeInference.py
@@ -20,22 +20,25 @@ import datetime
 	shift between architectures
 """
 
+# Check if CUDA enabled GPU is available else run on CPU
 if torch.cuda.is_available():
     print("\nYou have an ",torch.cuda.get_device_name(0)," (Cuda enabled GPU)")
-    GPU_FLAG = input("USE GPU (y/n) : ")
-    
+    GPU_FLAG = input("USE GPU (y/n) : ")    
 else:
     print("NO GPU found, Running on CPU!")
     GPU_FLAG = 'n'
+
+#Start time to calculate total inference time
 startime = time.time()
+
 parser = argparse.ArgumentParser(description="TSM Testing on real time!!")
 parser.add_argument('-f',type=str,help='Provide a video!!')
-parser.add_argument('--arch',type=str,help='provide architecture [mobilenetv2,resnet50]',default='resnet50')
-#Create Necessary Folders
+parser.add_argument('--arch',type=str,help='provide architecture [mobilenetv2,resnet50]',default='mobilenetv2')
+
 
 print()
 
-#just adding some comments to check git 
+# Parsing weights file to add temporal shift or not.
 def parse_shift_option_from_log_name(log_name):
     if 'shift' in log_name:
         strings = log_name.split('_')
@@ -46,14 +49,16 @@ def parse_shift_option_from_log_name(log_name):
     else:
         return False, None, None
 
-#args = parser.parse_args()
 args = vars(parser.parse_args())
+
+# switch between archs based on selected arch
 if args.get('arch') == 'mobilenetv2':
     this_weights='checkpoint/TSM_ucfcrime_RGB_mobilenetv2_shift8_blockres_avg_segment8_e25/ckpt.best.pth.tar'
 else:
     this_weights='checkpoint/TSM_ucfcrime_RGB_resnet50_shift8_blockres_avg_segment8_e25/ckpt.best.pth.tar'
 
 is_shift, shift_div, shift_place = parse_shift_option_from_log_name(this_weights)
+
 modality = 'RGB'
 
 if 'RGB' in this_weights:
@@ -63,6 +68,7 @@ if 'RGB' in this_weights:
 categories = ['Normal Activity','Abnormal Activity']
 num_class = len(categories)
 this_arch = args.get('arch')
+
 print("[INFO] >> Model loading weights from disk!!")
 
 net = TSN(num_class, 1, modality,
@@ -74,19 +80,19 @@ net = TSN(num_class, 1, modality,
               non_local='_nl' in this_weights,
               )
 
+# See GPU_FLAG to check where to load the weights on CPU or GPU
 if GPU_FLAG == 'y':
     checkpoint = torch.load(this_weights)
 else:
     checkpoint = torch.load(this_weights,map_location=torch.device('cpu'))
 
-
 checkpoint = checkpoint['state_dict']
 
-# base_dict = {('base_model.' + k).replace('base_model.fc', 'new_fc'): v for k, v in list(checkpoint.items())}
 base_dict = {'.'.join(k.split('.')[1:]): v for k, v in list(checkpoint.items())}
 replace_dict = {'base_model.classifier.weight': 'new_fc.weight',
                     'base_model.classifier.bias': 'new_fc.bias',
                }
+
 for k, v in replace_dict.items():
     if k in base_dict:
         base_dict[v] = base_dict.pop(k)
@@ -111,25 +117,20 @@ transform=torchvision.transforms.Compose([
 WINDOW_NAME = 'Real-Time Video Action Recognition'
 
 
-def saveSnapShot(img):
-    #img=cv2.flip(img,0)
+def getFileName(path,fileExtension):
     x = datetime.datetime.now()
-    path = './appData/Anoamly_Images/'
-    getImageName = path+str(x.strftime("%A") + "_" + str(x.date()) + " @ " + str(x.strftime("%I:%M:%S")))+'.jpg'
-    cv2.imwrite(getImageName,img)
+    getName = path+str(x.strftime("%A") + "_" + str(x.date()) + " @ " + str(x.strftime("%I:%M:%S")))+fileExtension
+    return getName
 
-#ToDo: Record anamolous event    
-def recordAnaomlousEvent(writer,frame):
-    pass
 
 #get max abnormal prob not an efficient way may use to much ram
 maxAbnormalProb =[-1]
 FpsList = []
 estFps = None
 maxFps = None
-def doInferecing(cap):
-    
-# set a lower resolution for speed up
+
+def doInferecing(cap): 
+    # set a lower resolution for speed up
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
@@ -139,28 +140,21 @@ def doInferecing(cap):
     cv2.resizeWindow(WINDOW_NAME, 640, 480)
     cv2.moveWindow(WINDOW_NAME, 0, 0)
     cv2.setWindowTitle(WINDOW_NAME, WINDOW_NAME)
+    
     t = None
     i_frame = -1
     count = 0
+    
     #variable to hold writer object
     writer = None
-    
-    
+
     print("Ready!")
 
-
-    c = 0
     while cap.isOpened():
         i_frame += 1
         hasFrame, img = cap.read()  # (480, 640, 3) 0 ~ 255
-        
-        
             
         if hasFrame:
-           
-            
-            #img=cv2.flip(img,180)
-            
             img_tran = transform([Image.fromarray(img).convert('RGB')])
             if i_frame % skip_frames == 0:  # skip every other frame to obtain a suitable frame rate  
                 t1 = time.time()
@@ -189,17 +183,13 @@ def doInferecing(cap):
                 current_time = t2 -t1
         
             img = cv2.resize(img, (640, 480))
-            #img = img[:, ::-1]
             height, width, _ = img.shape
-           # label = np.zeros([height // 5, width, 3]).astype('uint8') + 255
-        
+           
             if categories[idx[0]] == 'Abnormal Activity':
                 R = 255
                 G = 0  
                 Abnormality = True
-                #c+=1
                 tempThres = probs[0]
-                #recordAnaomlousEvent(writer,img)
                 maxAbnormalProb.append(float(probs[0]))
             else:
                 R = 0
@@ -229,31 +219,23 @@ def doInferecing(cap):
                        cv2.FONT_HERSHEY_SIMPLEX,
                        0.7, (0, 255, 255), 2)
                   
-            #img = np.concatenate((img, label), axis=0)
             if writer is None:
                 fourcc = cv2.VideoWriter_fourcc(*'XVID')
                 (H,W) = img.shape[:2]
-                x = datetime.datetime.now()
                 path = './appData/Anoamly_Clips/'
-                getVidName = path+str(x.strftime("%A") + "_" + str(x.date()) + " @ " + str(x.strftime("%I:%M:%S")))+'.avi'
+                getVidName = getFileName(path,'.avi')
                 writer = cv2.VideoWriter(getVidName, fourcc, 20.0, (W,H),True)
             
-            #Saving Anaomlous Event Image
+            #Saving Anaomlous Event Image and Clip
             if Abnormality:
                 writer.write(img)
                 if tempThres > 0.75:
-                    
-                    saveSnapShot(img)
+                    path = './appData/Anoamly_Images/'
+                    imageName = getFileName(path,'.jpg')
+                    cv2.imwrite(imageName,img)
             
             cv2.imshow(WINDOW_NAME, img)
             
-            
-            #writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'MJPG'),30,(W,H),True)
-            #img=cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
-            
-            #recordAnaomlousEvent(writer,img)
-            #writer.write(img)
-            #print('-'*20)
             key = cv2.waitKey(1)
         
             if key & 0xFF == ord('q') or key == 27:  # exit
@@ -275,7 +257,6 @@ def doInferecing(cap):
                 nt = time.time()
                 count += 1
                 t = nt
-            #exit(0)
         else:       
             #i_frame = 0
             #cap.set(cv2.CAP_PROP_POS_FRAMES,0)
@@ -285,35 +266,38 @@ def doInferecing(cap):
             #Clearing Variables for re-running
             #estFps=None
             #maxAbnormalProb.clear()
-            #maxFps=None
-            
-            
+            #maxFps=None   
+    # Calculating total execution time        
     execTime = time.time() - startime
     print()
     
+    # Display Results
     print('<<< [INFO] >>> Total Abnormal Probs : ',len(maxAbnormalProb))
     print('<<< [INFO] >>> Max Abnormality Prob : {:.2f}'.format(max(maxAbnormalProb)))
+    print('<<< [INFO] >>> Max Abnormality Prob : {:.2f}'.format(sum(maxAbnormalProb)/len(maxAbnormalProb)))
     print('<<< [INFO] >>> Max FPS achieved     : {:.1f}'.format(maxFps))
     print('<<< [INFO] >>> Averge Estimated FPS : {:.1f}'.format(estFps)) 
     print('<<< [INFO] >>> Total Infernece Time : {:.2f} seconds'.format(execTime))
     
 def main():
+    # Create necessary folder to hold data (Anoamly Clips and Images)
+    # make directories if donot exist else pass
     try:
         os.makedirs('./appData/Anoamly_Clips')
         os.makedirs('./appData/Anoamly_Images')
     except OSError as e:
         pass
+        
+    # if no argument is supplied open webcamera
     if not args.get('f', False):
         print("Openinig camera...")
         cap = cv2.VideoCapture(0)
-        
         #cap = cv2.VideoCapture('http://192.168.43.1:8080/video')
         
     else:
         print("loading Video...")
         cap = cv2.VideoCapture(args['f'])
-        
-        
+    #start inferencing in real time     
     doInferecing(cap)
 
 main()
